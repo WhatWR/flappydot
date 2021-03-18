abf6618a194366ef38770ceca3c55266d7963c97
pattanan-cyber <pattanan.pr@ku.th>
2021-03-18 22:29:17 +0700

    image

--- flappydot.py
+++ flappydot.py
@@ -83,9 +83,8 @@ class FlappyGame(GameApp):
     def create_sprites(self):
         self.image_filename = 'images/bgggg.png'
         self.photo_image = tk.PhotoImage(file=self.image_filename)
-        # image = image.resize((450, 350), Image.ANTIALIAS)
         self.canvas_object_id = self.canvas.create_image(250, 200, image=self.photo_image)
-        self.dot = Dot(self, 'images/angry-birds.png',CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
+        self.dot = Dot(self, 'images/yy.png',CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
         self.elements.append(self.dot)
         self.pillar_pair = PillarPair(self, 'images/pillar-pair.png', CANVAS_WIDTH, CANVAS_HEIGHT // 2)
         self.elements.append(self.pillar_pair)
